46dc50efaabfce993d09041979db812d364b464a
Gabjea <m.beaudoinmarc@gmail.com>
2026-02-18 15:35:53 +0100

    add websocket const

--- src/config.rs
+++ src/config.rs
@@ -1,8 +1,12 @@
 // BLOCKCHAIN
-pub const WSS_BLOCKCHAIN: &str="wss://arbitrum.drpc.org";
-pub const POOL_ADDRESS: &str="0xC6962004f452bE9203591991D15f6b388e09E8D0";
-pub const SWAP_TOPIC: &str="0xc42079f94a6350d7e6235f29174924f928cc2ac818eb64fed8004e115fbcca67";
+pub const WSS_BLOCKCHAIN: &str = "wss://arbitrum.drpc.org";
+pub const POOL_ADDRESS: &str = "0xC6962004f452bE9203591991D15f6b388e09E8D0";
+pub const SWAP_TOPIC: &str = "0xc42079f94a6350d7e6235f29174924f928cc2ac818eb64fed8004e115fbcca67";
 
 // HYPERLIQUID
-pub const WSS_HYPERLIQUID: &str="wss://api.hyperliquid.xyz/ws";
-pub const COIN: &str="ETH";
+pub const WSS_HYPERLIQUID: &str = "wss://api.hyperliquid.xyz/ws";
+pub const COIN: &str = "ETH";
+
+// WEBSOCKET
+pub const IP_ADDRESS: &str = "127.0.0.1";
+pub const PORT: u16 = 9001;
