55966a5621f6f563f4acbb095e41d556e167fc67
Gabjea <m.beaudoinmarc@gmail.com>
2026-02-18 15:37:28 +0100

    prod ready

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,33 @@
-mod feed;
 mod config;
+mod feed;
 use feed::LiveEvent;
-use tokio::sync::mpsc;
+mod state;
+use state::{StdEvent, create_std_event};
+mod websocket;
+use websocket::start_websocket_server;
+
+use tokio::sync::{broadcast, mpsc};
 
 #[tokio::main]
 async fn main() {
+    let mut market = state::market_state::MarketState::default();
+
     // Initialize & Run LiveFeed
     let (tx, mut rx) = mpsc::channel::<LiveEvent>(1024);
     tokio::spawn(feed::start_livefeed(tx));
-    
+
+    // Initialize & Run Webscoket
+    let (ws_tx, _) = broadcast::channel::<StdEvent>(1024);
+    let ws_tx_clone = ws_tx.clone();
+    tokio::spawn(async move {
+        start_websocket_server(ws_tx_clone).await;
+    });
+
     // Central Loop : Triggered when Event received
     while let Some(event) = rx.recv().await {
-        
-        match event {
-            LiveEvent::Uniswap {
-                block_number,
-                amount0 : amo,
-                .. } => {
-                println!("[UNI]: {}, {:?}",block_number,amo);
-            }
-            LiveEvent::Hyperliquid { 
-                timestamp,
-                .. } => {
-                println!("[HYPER] {}",timestamp);
-            }
+        if market.update(&event) {
+            let event_to_broadcast = create_std_event(&market, event);
+            let _ = ws_tx.send(event_to_broadcast);
         }
     }
 }
